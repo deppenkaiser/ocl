e6e06af4758d2b842582421b7ac75878a7872709
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2026-05-20 18:29:37 +0200

    ...

--- kernels.h
+++ kernels.h
@@ -8,3 +8,4 @@ protected_import(const char*, ocl_get_source_brightest_spot(void));
 protected_import(const char*, ocl_get_source_matvec_bf16(void));
 protected_import(const char*, ocl_get_source_matvec_bf16_fused(void));
 protected_import(const char*, ocl_get_source_matvec_f32(void));
+protected_import(const char*, ocl_get_source_sd_attention_f32(void));
--- ocl.c
+++ ocl.c
@@ -14,7 +14,8 @@ private const char *_ocl_kernel_names[OCL_KERNEL_COUNT] =
 	"brightest_spot",
 	"matvec_bf16",
 	"matvec_bf16_fused",
-	"matvec_f32"
+	"matvec_f32",
+	"sd_attention_f32"
 };
 
 bool ocl_initialize(const ocl_core_t ocl)
@@ -250,6 +251,7 @@ private char *_ocl_build_source(void)
 		ocl_get_source_matvec_bf16(),
 		ocl_get_source_matvec_bf16_fused(),
 		ocl_get_source_matvec_f32(),
+		ocl_get_source_sd_attention_f32(),
 		NULL
 	};
 
